e346570633dabf6407c4a27c65bf9ed18625b127
Kurvaaa <93648377+bosniankicks@users.noreply.github.com>
2024-11-03 14:37:28 -0600

    Create init.js

---
+++ init.js
@@ -0,0 +1,32 @@
+ndwti({
+    "wi": "w-481390",
+    "co": {
+        "useNdx": true
+    },
+    "wmd": {
+        "ipr": {
+            "fm": [],
+            "lm": true,
+            "tl": 5000,
+            "pd": {
+                "mn": "0",
+                "iq": "0"
+            },
+            "il": 25
+        },
+        "wk": {
+            "r": "test"
+        },
+        "di": {
+            "rt": 128,
+            "ut": 512
+        },
+        "af": []
+    },
+    "fd": {
+        "ipr": "p",
+        "bi": "p",
+        "wt": "1.w-481390.1.2.1vfWm5aEjMNFVNmtFDMMxw,,.TMoCUbaq6f9gS6NLGpXIAra_HfCrN7klWa3XZ5LYq_9SsADbgaizmNLAJgqklZwIhDCzrVZfWBI8778TM8lrwxn_d9RnCmy3CYB3NnP8zSpHYxwL5KHibfRXJsFHkuqfsj_DWQm9Jw7tpY46m5LYNhEVr7yDTpJAVBxGy-8bF1wS8l7_N1kH5_IX5EMIDZ9C5NVWP5Eq1rjl46nh4zEanJabMg8nk1btU61IxsnaQO6T8xlHu_ti6MJXolK5jrUXnI2SAX72BkXtPyYb-tzZXQ,,"
+    },
+    "gf": []
+})
